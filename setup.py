7fa5c218d6aefa2f87b8de36938221f640cb0f3e
Siavash Yasini <yasini@usc.edu>
2020-01-25 15:25:43 -0800

    Add requirements and update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as f:
+    reqs = [line.rstrip("\n") for line in f if line != "\n"]
+
 setup(
-    name='arXiv',
+    name='arxivester',
     version='0.1',
-    packages=['arxiv'],
+    packages=['arxivester'],
+    install_requires=reqs,
     url='https://github.com/syasini/arXiv',
     license='',
     author='Siavash Yasini, Amin Oji',
     author_email='siavash.yasini@gmail.com',
     description='a python code for scraping arXiv and inSPIRE-HEP.'
     )
+
